cff93ccadbf7b83c36a54e8c9106e66927d271ef
Jacob Banks <jacobbanks.dev@gmail.com>
2025-08-23 17:19:18 -0700

    Kube Config Parsing

--- cmd/kubeoptic/main.go
+++ cmd/kubeoptic/main.go
@@ -1,3 +1,35 @@
 package main
 
+import (
+	// "fmt"
+	"path/filepath"
+
+	"flag"
+	"kubeoptic/internal/k8s"
+	"kubeoptic/internal/models"
+
+	// v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	// kube "k8s.io/client-go/kubernetes"
+	// cmd "k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
 // TODO: Implement main entry point
+
+
+func main() {
+	var configpath *string
+	if home := homedir.HomeDir(); home != "" {
+		configpath = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to kube config file")
+	} else {
+		configpath = flag.String("kubeconfig", "", "absolute path to kube config")
+	}
+
+	flag.Parse()
+	ko := models.NewKubeOptic(configpath)
+	k8s.LoadDataFromConfig(ko)
+
+
+
+	// k8s.CountPods()
+}
